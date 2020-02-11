86c2478c630ffc5110f3c87d70082b932ac4bdd4
marcio1002 <marcioalemao1999@yahoo.com>
2020-02-10 23:21:14 -0300

    ..

--- src/delete.php
+++ src/delete.php
@@ -4,9 +4,6 @@
 
     $account = new AccountController();
 
-    $name = "Jaqueline Almeida";
-    $email = "jaquelinealmeida@gmail.com";
-    $update = ["nome = '$name'","email = '$email'"];
     try{
         $result =  $account->deleteRegistry($connection,"usuarios","id_usuarios = 5");
         echo "<script> alert('Ok') </script>";
